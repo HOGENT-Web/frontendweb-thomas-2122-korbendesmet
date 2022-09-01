789e1fa7691323252f72e2c52d131a7630650d6c
KorbenDeSmet <korbendesmet@hotmail.com>
2022-09-01 10:10:51 +0200

    Revert "Added login form animation"
    This reverts commit 7cc8dfceb2ae9e9c5bb5a23fc13c857937be796f.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,7 @@ import { useLogin, useLogout, useSession } from './../contexts/AuthenticationPro
 
 
 const Header = () => {
+    const [enabled, changeState] = useState(false);
     let nav = useNavigate();
     const methods = useForm();
     const { handleSubmit, register } = methods;
@@ -16,17 +17,13 @@ const Header = () => {
     const logout = useLogout();
     const { isAuthed, loading } = useSession();
 
-    const toggleView = () => {
-        document.getElementById('formBox').classList.toggle('-translate-x-72');
-    }
-
     //TODO: Error if login is false and not all fields in use
     const onSubmit = useCallback(async (data) => {
         document.getElementById("errorLogin").hidden = true;
         const temp = await login(data.email, data.wachtwoord);
         if (temp === false) return document.getElementById("errorLogin").hidden = false;
-        toggleView();
-    }, [login]);
+        changeState(!enabled)
+    }, [enabled, login]);
 
     const handleLogout = useCallback(() => {
         logout();
@@ -36,42 +33,45 @@ const Header = () => {
         <div id="header">
             {
                 !isAuthed ?
-                    <>
+                    <div>
                         <div className="flex justify-between py-3 px-7 md:px-24 bg-neutral-900">
                             <div className="relative w-52">
                                 <img onClick={() => nav('/')} src={logo} className="object-contain hover:cursor-pointer" alt="Green uk oil logo" />
                             </div>
                             <div className="my-auto">
-                                <button id="loginButton" onClick={toggleView} className="text-white text-xl p-2 hover:bg-neutral-800 rounded-lg border border-neutral-900 hover:border-neutral-700">Log in</button>
+                                <button id="loginButton" onClick={() => changeState(!enabled)} className="text-white text-xl p-2 hover:bg-neutral-800 rounded-lg border border-neutral-900 hover:border-neutral-700">Log in</button>
                             </div>
                         </div>
-                        <div id="formBox" className="fixed -right-72 duration-500 top-28 rounded-l-lg bg-neutral-900 h-fit">
-                            <FormProvider {...methods}>
-                                <form onSubmit={handleSubmit(onSubmit)} id="form" className="mx-3 text-sm text-neutral-200">
-                                    <label className="block ml-1 mt-2">Email: </label>
-                                    <input {...register('email')} id="email" type="text" label="email" className="w-full bg-neutral-900 border border-neutral-200 rounded-lg p-1" />
-                                    <label className="block ml-1 mt-2">Wachtwoord: </label>
-                                    <input {...register('wachtwoord')} id="password" type="password" label="wachtwoord" className="w-full bg-neutral-900 border border-neutral-200 rounded-lg p-1" />
-                                    <input id="submit" type="submit" disabled={loading} value="inloggen" className="underline hover:bg-neutral-800 bg-neutral-900 border border-neutral-900 hover:border-neutral-200 rounded-lg p-1 px-2 mt-3 float-right mb-2 italic" />
-                                    <div id="errorLogin" hidden={true} className="text-red-400 text-sm mt-4">Verkeerde gegevens!</div>
-                                </form>
-                            </FormProvider>
-                        </div>
-                    </>
+                        {
+                            enabled ?
+                                <div className="absolute top-28 right-0 rounded-l-lg bg-neutral-900 h-fit">
+                                    <FormProvider {...methods}>
+                                        <form onSubmit={handleSubmit(onSubmit)} id="form" className="mx-3 text-sm text-neutral-200">
+                                            <label className="block ml-1 mt-2">Email: </label>
+                                            <input {...register('email')} id="email" type="text" label="email" className="w-full bg-neutral-900 border border-neutral-200 rounded-lg p-1" />
+                                            <label className="block ml-1 mt-2">Wachtwoord: </label>
+                                            <input {...register('wachtwoord')} id="password" type="password" label="wachtwoord" className="w-full bg-neutral-900 border border-neutral-200 rounded-lg p-1" />
+                                            <input id="submit" type="submit" disabled={loading} value="inloggen" className="underline hover:bg-neutral-800 bg-neutral-900 border border-neutral-900 hover:border-neutral-200 rounded-lg p-1 px-2 mt-3 float-right mb-2 italic" />
+                                            <div id="errorLogin" hidden={true} className="text-red-400 text-sm mt-4">Verkeerde gegevens!</div>
+                                        </form>
+                                    </FormProvider>
+                                </div> : null
+                        }
+                    </div>
                     :
-                    <>
+                    <div>
                         <div className="flex justify-between py-3 px-7 md:px-24 bg-neutral-900">
                             <div className="relative w-52">
                                 <img onClick={() => nav('/')} src={logo} className="object-contain hover:cursor-pointer" alt="Green uk oil logo" />
                             </div>
                             <div className="my-auto">
                                 <button id="menuButton" onClick={() => nav('/admin')} className="text-white text-xl p-2 hover:bg-neutral-800 rounded-lg border border-neutral-900 hover:border-neutral-700">Menu</button>
-                                <button id="logoutButton" onClick={() => handleLogout()} className="text-white text-xl p-2 hover:bg-neutral-800 rounded-lg border border-neutral-900 hover:border-neutral-700">Log uit</button>
+                                <button id="logoutButton"  onClick={() => handleLogout()} className="text-white text-xl p-2 hover:bg-neutral-800 rounded-lg border border-neutral-900 hover:border-neutral-700">Log uit</button>
                             </div>
                         </div>
-                    </>
+                    </div>
             }
-        </div >
+        </div>
     )
 }
 
